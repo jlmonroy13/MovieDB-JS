d4e6805646f56807774079834fd5f30be30b3236
Jorge Monroy <jlmonroy13@gmail.com>
2016-04-10 23:50:04 -0500

    info of each movie ok

--- application/javascript/api-moviedb.js
+++ application/javascript/api-moviedb.js
@@ -1,69 +1,52 @@
-// function getMovies(url) {
-//   return new Promise(function(resolve) {
-//     $.ajax({ type: 'GET',
-//       url: url,
-//       // async: false,
-//       success: function(data) {
-//         resolve(data);
-//       }
-//     });
-//   })
-// }
+function getMovies(completeurl, api_key) {
+  var url_moreInfo        =     'http://api.themoviedb.org/3/movie/',
+      template_movieleft  =     Handlebars.compile($('#movie-left-template').html()),
+      template_movieinfo  =     Handlebars.compile($('#movie-info-template').html());
 
 
+  $.get(completeurl).then(function(movies) {
 
+    $.get(url_moreInfo+movies.results[0].id+'/videos?api_key='+api_key).then(function(videos){
+      movies.results[0].trailer = videos.results[0].key;
+      return $.get(url_moreInfo+movies.results[0].id+'/similar?api_key='+api_key);
+    }).then(function(similars){
+      movies.results[0].similars = similars.results;
+      // //Change the main background image
+      $('.main').css('background', 'url("http://image.tmdb.org/t/p/w1920'+movies.results[0].backdrop_path+'") no-repeat');
+      //Insert trailer, overview and similars movies into infomovie sectionº
+      $('.js-movie-info').html(template_movieinfo(movies.results[0]));
+    });
 
-function getMovies(url, api_key, index) {
-  var url_moreInfo = 'http://api.themoviedb.org/3/movie/';
-  var data;
-  //GET THE MOVIE LIST
-  return $.get(url).then(function(movies) {
-    data = movies;
-    return $.get(url_moreInfo+movies.results[index].id+'/videos?api_key='+api_key);
-  }).then(function(videos) {
-    data.results[index].trailer = videos.results[0].key;
-    return $.get(url_moreInfo+data.results[index].id+'/similar?api_key='+api_key);
-  }).then(function(similars) {
-    data.results[index].similars = similars.results;
-    return data;
+    //add trailer, similars movies and functions to each object
+    $('.js-movie-container').empty();
+    $.each(movies.results, function(i, data) {
+      //Add node into the Object
+      data.$leftMovie = $(template_movieleft(data));
+      $('.js-movie-container').append(data.$leftMovie);
+      //Display movie info when you click a movie on the left menu
+      (function getTrailerAndSimilars() {
+        data.$leftMovie.click(function() {
+          $.get(url_moreInfo+data.id+'/videos?api_key='+api_key).then(function(videos){
+            data.trailer = videos.results[0].key;
+            return $.get(url_moreInfo+data.id+'/similar?api_key='+api_key);
+          }).then(function(similars){
+            data.similars = similars.results;
+            // //Change the main background image
+            $('.main').css('background', 'url("http://image.tmdb.org/t/p/w1920'+data.backdrop_path+'") no-repeat');
+            //Insert trailer, overview and similars movies into infomovie section
+            data.$infoMovie = $(template_movieinfo(data));
+            //Add to favorite
+            (function addFavorite() {
+              var $btn_favorite = data.$infoMovie.find('.js-btn-add');
+              $btn_favorite.click(function(){
+                data.favorite = true;
+              });
+            })();
+            $('.js-movie-info').html(data.$infoMovie);
+          });
+        });
+      })();
+    });
+    console.log(movies);
   });
 }
-
-
-// function getMovies(url) {
-//   var info;
-//   $.ajax({ type: 'GET',
-//     url: url,
-//     async: false,
-//     success: function(data) {
-//       info = data;
-//     }
-//   });
-//   return info;
-// }
-
-  // getMovies(now_playing+api_key).then(function(data) {
-  //   console.log(data);
-  //   //Insert movies on left Menu
-  //   $('.js-movie-container').html(template_movieleft(data));
-    
-  //   //Change the main background image
-  //   $('.main').css('background', 'url("http://image.tmdb.org/t/p/w1920'+data.results[0].backdrop_path+'") no-repeat');
-    
-  //   //Get the first movie trailer
-  //   var id_firstMovie = data.results[0].id;
-  //   var url_videos = 'http://api.themoviedb.org/3/movie/'+id_firstMovie+'/videos?api_key='+api_key;
-  //   getMovies(url_videos).then(function(videos) {
-  //     data.results[0].trailer = videos.results[1].key;
-  //   });
-    
-  //   //Get similar movies of the first movie
-  //   var url_similars = 'http://api.themoviedb.org/3/movie/'+id_firstMovie+'/similar?api_key='+api_key;
-  //   getMovies(url_similars).then(function(similars) {
-  //     data.results[0].similars = similars.results;
-  //   });
-
-  //   //Insert trailer, overview and similars movies into infomovie section
-  //   $('.js-movie-info').html(template_movieinfo(data.results[0]));
-  // });
-
--- application/javascript/application.js
+++ application/javascript/application.js
@@ -1,94 +1,42 @@
 $(function() {
   var $button_menu        =     $('.js-btn-menu'),
       $left_menu          =     $('.js-left-menu'),
+      $categories         =     $('.js-categories'),
       api_key             =     'c0b2e256491361d28c75bbe8f9e59a85',
-      template_movieleft  =     Handlebars.compile($('#movie-left-template').html()),
-      template_movieinfo  =     Handlebars.compile($('#movie-info-template').html()),
       now_playing         =     'https://api.themoviedb.org/3/movie/now_playing?api_key=',
       popular             =     'https://api.themoviedb.org/3/movie/popular?api_key=',
       top_rated           =     'https://api.themoviedb.org/3/movie/top_rated?api_key=',
       upcoming            =     'https://api.themoviedb.org/3/movie/upcoming?api_key=',
-      movies              =     [];
+      movies              =     [],
+      completeurl         =     now_playing+api_key;
 
-      var completeurl = top_rated+api_key;
   //To open the left Menu
   $button_menu.click(function() {
     $left_menu.toggleClass('left-menu--display');
   });
-
-
-   
-    getMovies(completeurl, api_key, 0).then(function(data) {
-      movies = data;
-      //Insert movies on left Menu
-      $('.js-movie-container').html(template_movieleft(movies));
-      //Change the main background image
-      $('.main').css('background', 'url("http://image.tmdb.org/t/p/w1920'+movies.results[0].backdrop_path+'") no-repeat');
-      //Insert trailer, overview and similars movies into infomovie section
-      $('.js-movie-info').html(template_movieinfo(movies.results[0]));
-    });
-
-
- 
-  // //Insert movies on left Menu
-  // movies = getMovies(now_playing+api_key);
-  // console.log(movies);
-  // $('.js-movie-container').html(template_movieleft(movies));
-
-  // //Change the main background image
-  // $('.main').css('background', 'url("http://image.tmdb.org/t/p/w1920'+movies.results[0].backdrop_path+'") no-repeat');
   
-  // //Get the first movie trailer
-  // var id_firstMovie = movies.results[0].id;
-  // var url_videos = 'http://api.themoviedb.org/3/movie/'+id_firstMovie+'/videos?api_key='+api_key;
-  // var videos = getMovies(url_videos);
-  // console.log(videos);
-  // movies.results[0].trailer = videos.results[0].key;
+  getMovies(completeurl, api_key);
 
-  // //Get similar movies of the first movie
-  // var url_similars = 'http://api.themoviedb.org/3/movie/'+id_firstMovie+'/similar?api_key='+api_key;
-  // var similars = getMovies(url_similars);
-  // movies.results[0].similars = similars.results;
-  // console.log(similars);
-
-  // //Insert trailer, overview and similars movies into infomovie section
-  // $('.js-movie-info').html(template_movieinfo(movies.results[0]));
-
-
-
-
-
-  // var condicion = true;
-
-  //CREATING A PROMISE
-  // var promise = new Promise(function (resolve, reject) {
-  //   if(condicion) {
-  //     resolve('Sí, todo funcionó');
-  //   }
-  //   else {
-  //     reject(new Error('Algo anda mal'));
-  //   }
-  // });
-  // promise.then(function (result) {
-  //   console.log('¿Funcionó?', result);
-  // }).catch(function(err) {
-  //   console.error('ERR', err);
-  // })
-
-
-  // function wait(ms) {
-  //   return new Promise(function(resolve) {
-  //     console.log(this);
-  //     window.setTimeout(function() {
-  //       resolve();
-  //     }, ms);
-  //   });
-  // };
-
-  // var milliseconds = 2000;
-  // wait(milliseconds).then(alert(milliseconds));
-  
-  
-
+  //GET MOVIES
+  $categories.change(function(){
+    var selectedOption = $('.js-categories option:selected');
+    /*NOW PLAYING SELECTION---------------------------------------------------------------------*/  
+    if( selectedOption.val() == 'now_playing') {
+      completeurl = now_playing+api_key;
+      getMovies(completeurl, api_key);
+    /*POPULAR SELECTION---------------------------------------------------------------------*/  
+    }else if(selectedOption.val() == 'popular') {
+      completeurl = popular+api_key;
+      getMovies(completeurl, api_key);
+    /*UPCOMING SELECTION---------------------------------------------------------------------*/     
+    }else if(selectedOption.val() == 'upcoming') {
+      completeurl = upcoming+api_key;
+      getMovies(completeurl, api_key);
+    /*TOP RELATED SELECTION---------------------------------------------------------------------*/      
+    }else if(selectedOption.val() == 'top_rated') {
+      completeurl = top_rated+api_key;
+      getMovies(completeurl, api_key);
+    }
+  });
 });//End Document Ready
 
